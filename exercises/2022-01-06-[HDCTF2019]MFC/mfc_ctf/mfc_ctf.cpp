d8ec156cde8990197ceb04ba884bc525e7e8b279
yqw1212 <55695639+yqw1212@users.noreply.github.com>
2022-01-06 18:34:35 +0800

    add 2022-01-06

---
+++ exercises/2022-01-06-[HDCTF2019]MFC/mfc_ctf/mfc_ctf.cpp
@@ -0,0 +1,17 @@
+﻿// mfc_ctf.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <stdio.h>
+
+int main()
+{
+	HWND h = FindWindowA(NULL, "Flag就在控件里");
+	if (h){
+		SendMessage(h, 0x0464, 0, 0);
+		printf("success");
+	} else {
+		printf("failure");
+	}
+}
