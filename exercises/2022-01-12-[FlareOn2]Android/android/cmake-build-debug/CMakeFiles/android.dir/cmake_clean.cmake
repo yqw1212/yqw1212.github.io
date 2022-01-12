432de9a5a3520d03416e09c20045da993f517c11
yqw1212 <55695639+yqw1212@users.noreply.github.com>
2022-01-12 20:20:59 +0800

    add 2022-01-12

---
+++ exercises/2022-01-12-[FlareOn2]Android/android/cmake-build-debug/CMakeFiles/android.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/android.dir/main.cpp.obj"
+  "android.exe"
+  "android.exe.manifest"
+  "android.pdb"
+  "libandroid.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/android.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
